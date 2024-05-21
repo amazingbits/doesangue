b87c8b3f766b7bb08b04d0ec171bff083c54484d
Guilherme Andrade <gui-keanne@hotmail.com>
2024-05-21 09:16:53 -0300

    padronizando os arquivos server.js e iews/index.html às mudanças na configuração do banco de dados

--- server.js
+++ server.js
@@ -26,16 +26,17 @@ nunjucks.configure('views', {
 const conexao = mysql.createConnection({
     host: 'localhost',
     //seu usuário aqui
-    user: 'SEU USER',
+    user: 'root',
     //sua senha aqui
-    password: 'SUA SENHA',
-    database: 'doe'
+    password: 'root',
+    database: 'db_doesangue',
+    port: 3307
 })
 
 //rota principal
 server.get('/', function(req, res) {
     //selecionar os itens do banco
-    const sql = 'SELECT * FROM `DOADOR` ORDER BY ID DESC'
+    const sql = 'SELECT * FROM `tbl_doador` ORDER BY id DESC'
     conexao.execute(sql, function(err, results, fields){
         if(err) return res.send('Erro com o banco de dados...')
         const dados = results
@@ -56,7 +57,7 @@ server.post('/', function(req, res) {
 
     //se passar pela regra, segue o programa
     //inserimos os dados no banco
-    const sqlInsert = 'INSERT INTO `DOADOR` VALUES (null, ?, ?, ?)'
+    const sqlInsert = 'INSERT INTO `tbl_doador` VALUES (null, ?, ?, ?)'
     conexao.execute(sqlInsert, [nome, email, sangue], function(err, results, fields){
         if(err) return res.render('error')
         return res.redirect('/')
